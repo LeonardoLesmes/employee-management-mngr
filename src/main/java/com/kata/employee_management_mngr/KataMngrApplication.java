b695ebf9ba6b90a388f0e4f216f6edba776d227f
Leonardo <leonardo_lesmes@outlook.com>
2025-05-31 00:41:20 -0500

    feat: Init-mngr - Added basic structure, entry point, dependencies and config

---
+++ src/main/java/com/kata/employee_management_mngr/KataMngrApplication.java
@@ -0,0 +1,13 @@
+package com.kata.employee_management_mngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataMngrApplication.class, args);
+	}
+
+}
