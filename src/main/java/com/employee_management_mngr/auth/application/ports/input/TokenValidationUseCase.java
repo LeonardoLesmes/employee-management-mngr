fcf7d5c71e6d7f6a9113f1ce06775c4286635b0c
Leonardo <leonardo_lesmes@outlook.com>
2025-06-03 20:17:24 -0500

    refactor: remove unnecessary comments from TokenValidationUseCase interface

--- src/main/java/com/employee_management_mngr/auth/application/ports/input/TokenValidationUseCase.java
+++ src/main/java/com/employee_management_mngr/auth/application/ports/input/TokenValidationUseCase.java
@@ -1,13 +1,5 @@
 package com.employee_management_mngr.auth.application.ports.input;
 
 public interface TokenValidationUseCase {
-    /**
-     * Validates if the given JWT token is valid
-     *
-     * @param token
-     *            The JWT token to validate
-     *
-     * @return true if the token is valid, false otherwise
-     */
     boolean validateToken(String token);
 }
